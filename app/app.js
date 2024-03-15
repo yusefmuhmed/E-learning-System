6f3af9b501b753cca620e08db9cb15b49e92c88f
yusefmuhmed <yusefmuhmedd6766@gmail.com>
2024-03-15 23:49:16 +0200

    Enhance Project to fit the new requirements

--- app/app.js
+++ app/app.js
@@ -6,16 +6,9 @@ app.use(cors())
 require("../db/connect")
 app.use(express.json())
 app.use(express.static(path.join(__dirname, "../uploads")))
+
 const userRoutes = require("../routes/user.routes")
-const postRoutes = require("../routes/post.routes")
-const projectRoutes = require("../routes/project.routes")
-const schoolRoutes = require("../routes/school.routes.js")
-
-
 app.use("/api/user/", userRoutes)
-app.use("/api/post/", postRoutes)
-app.use("/api/project/", projectRoutes)
-app.use("/api/school/", schoolRoutes)
 
 app.all("*", (req, res) => {
     res.status(404).send({
--- routes/project.routes.js
+++
@@ -1,8 +0,0 @@
-const router = require("express").Router()
-const Project = require('../app/controller/project.controller')
-const { auth } = require("../app/middleware/auth.middleware")
-
-router.post("/addProject", Project.insertProject)
-
-
-module.exports = router
