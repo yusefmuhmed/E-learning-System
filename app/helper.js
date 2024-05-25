879436d17e39c1915b46e778f70947b2e5cd1baa
yusefmuhmed <yusefmuhmedd6766@gmail.com>
2024-05-25 10:56:27 +0300

    F

--- app/helper.js
+++ app/helper.js
@@ -12,48 +12,50 @@ class MyHelper {
 
   static emailHandler = (mail) => {
     return new Promise((resolve, reject) => {
-        const otp = otpGenerator.generate(4, {
-            digits: true,
-            lowerCaseAlphabets: false,
-            upperCaseAlphabets: false,
-            specialChars: false,
-        });
+      const otp = otpGenerator.generate(4, {
+        digits: true,
+        lowerCaseAlphabets: false,
+        upperCaseAlphabets: false,
+        specialChars: false,
+      });
 
-        const transporter = nodemailer.createTransport({
-            host: "smtp-mail.outlook.com",
-            port: 587,
-            secure: false,
-            auth: {
-                user: "youssefmohamed3011@outlook.com",
-                pass: "Photoshop30112556",
-            },
-        });
+      const transporter = nodemailer.createTransport({
+        host: "smtp-mail.outlook.com",
+        port: 587,
+        secure: false,
+        auth: {
+          user: "TamerGomaa_71@outlook.com",
+          pass: "explain@1234",
+        },
+        tls: {
+          rejectUnauthorized: false,
+        },
+      });
 
-        const mailOptions = {
-            from: "youssefmohamed3011@outlook.com",
-            to: mail,
-            subject: "Password Reset OTP",
-            text: `Your OTP for password reset is: ${otp}`,
-        };
+      const mailOptions = {
+        from: "TamerGomaa_71@outlook.com",
+        to: mail,
+        subject: "Password Reset OTP",
+        text: `Your OTP for password reset is: ${otp}`,
+      };
 
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                console.error("Error sending email:", error);
-                reject({
-                    apiStatus: false,
-                    data: null,
-                    message: "Error sending email",
-                });
-            } else {
-                resolve({
-                    apiStatus: true,
-                    data: info.response,
-                    otp: otp,
-                });
-            }
-        });
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.error("Error sending email:", error);
+          reject({
+            apiStatus: false,
+            data: null,
+            message: "Error sending email",
+          });
+        } else {
+          resolve({
+            apiStatus: true,
+            data: info.response,
+            otp: otp,
+          });
+        }
+      });
     });
-};
-
+  };
 }
 module.exports = MyHelper;
